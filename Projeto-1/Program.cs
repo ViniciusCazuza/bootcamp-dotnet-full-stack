77e6893d215a39391bf25f3f45c565f10760822c
Vinicius Cazuza <cazuza.dev@gmail.com>
2024-08-27 18:54:54 -0300

    feat: Adição cadastro Nome e Data de Nascimento, verificação de idade e validação de caracter.

--- Projeto-1/Program.cs
+++ Projeto-1/Program.cs
@@ -1,16 +1,69 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using PessoaFisica.models;
+
+
+Pessoa pessoa1 = new Pessoa();
+DataNascimento data = new DataNascimento();
+DataNascimento error = new DataNascimento();
+
+
+Console.WriteLine($"\n\nOlá, Qual é o seu Primeiro Nome ? \n\n");
+
+pessoa1.Nome = Console.ReadLine();
+string nome = pessoa1.Nome;
+var errorMessage = false;
+Console.WriteLine($"\n\nÓtimo, agora qual é o seu Segundo Nome ? \n\n");
+
+pessoa1.Sobrenome = Console.ReadLine();
+
+
+pessoa1.CadastroNome();
 
 
 
-namespace Bootcamp_Dio 
-{
-    class Program 
+    Console.WriteLine($"\nAgora vamos cadastrar a sua Data de Nascimento.\n");
+    while (errorMessage == false ) 
     {
-        static void Main(string[] args)
+        Console.WriteLine($"\n\nEm que dia você Nasceu ? \n\n");
+        var respostaDia = Console.ReadLine();
+        errorMessage = int.TryParse(respostaDia, out int b);
+
+        if (errorMessage == true)
         {
-            Console.WriteLine("Hello, World");
+
+            data.Dia = Convert.ToInt32(respostaDia);
+        
+        }
+
+        Console.WriteLine($"\n{nome}, agora me diga o mês em que você Nasceu ? \n\n");
+        var respostaMes = Console.ReadLine();
+        errorMessage = int.TryParse(respostaMes, out b);
+
+        if (errorMessage == true)
+        {
+            
+            data.Mes = Convert.ToInt32(respostaMes);
+
+        }
+
+
+        Console.WriteLine($"\n{nome}, Por favor me diga o ano que você Nasceu ? \n\n");
+        var respostaAno = Console.ReadLine();
+        errorMessage = int.TryParse(respostaAno, out b);
+        if (errorMessage == true)
+        {
+
+            data.Ano = Convert.ToInt32(respostaAno);
+        
+        }
+        data.CadastroData();
+
+        if (data.Dia == 0 || data.Mes == 0 || data.Ano == 0)
+        {
+            errorMessage = false;
+
         }
     }
-}
+
+
+
+
